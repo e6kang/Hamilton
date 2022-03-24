87740c7120bdd4e4e40e2bd145af5209773f5f27
e6kang <71520955+e6kang@users.noreply.github.com>
2022-03-24 09:19:05 -0700

    Edit how excel is read

--- streamlit_ex2.py
+++ streamlit_ex2.py
@@ -220,7 +220,7 @@ def main():
             if data_file.name.endswith('csv'):
                 df = pd.read_csv(data_file)
             elif data_file.name.endswith('xlsx'):
-                df = pd.ExcelFile(data_file).parse()
+                df = pd.ExcelFile(data_file)
             
             # Plot hit picking
             f, ax = plt.subplots(figsize = (5, 3))
